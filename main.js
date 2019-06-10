6e1c68f3e47f043346f1d556c4a3be1e0d063d5f
Karol Skalutki| <skalutki@gmail.com>
2019-06-10 21:22:19 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const age = 31;
+const name = 'Karol';
+
+console.log(`Hej tu ${name} mam ${age} lat cieszę się, że zaglądasz w to miejsce, dzięki temu mogę Cię poinformować, że jest to moja pierwsza strona internetowa w życiu !`);
