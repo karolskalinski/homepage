f6201422d912ea79c4f0cc4907def729e4906d61
Karol Skalutki| <skalutki@gmail.com>
2019-07-01 20:09:44 +0100

    feat: @media query and grid

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-const age = 31;
-const name = 'Karol';
+const welcome = (name, age) => {
+    console.log(`Witaj ${name}, masz ${age} lat!`);
+}
 
-console.log(`Hej tu ${name} mam ${age} lat cieszę się, że zaglądasz w to miejsce, dzięki temu mogę Cię poinformować, że jest to moja pierwsza strona internetowa w życiu !`);
-
-const heading = document.querySelector('.main__heading--js');
-console.log(heading.innerHTML);
-
-
-
-const daily__pharagraph = document.querySelectorAll('.daily__pharagraph');
-console.log(daily__pharagraph);
-console.log(daily__pharagraph[0]);
-
-const testjs = document.querySelector('.about__box--js')
-testjs.innerHTML = 'Zapełniamy ten element z poziomu JS';
+welcome("Karol", 30);
